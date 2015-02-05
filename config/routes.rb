d0aef838dd34c1182c9fe000d55660fcb0dd7448
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-02-04 20:43:13 -0500

    Intial setup of teacher

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'teacher#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :teachers
+
+  get 'page/index'
+  root 'page#index'
+end
